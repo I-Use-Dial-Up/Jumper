e0473d4906607c8a784aef00a120fe2c1b3c6006
I-Use-Dial-Up <danielgonzalez@hilltoprobotics.com>
2026-04-29 15:44:22 -0700

    Update CoinCounter.cs

--- CoinCounter.cs
+++ CoinCounter.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 
 public CoinCounter scoreText;
+private int score = 0;
+
+
+
 
 public class CoinCounter
 {
-	public CoinCounter()
+	public UpdateCoinCounter(int coins)
 	{
 
 
