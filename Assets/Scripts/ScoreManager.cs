90e4c8c86f485ed7e597703825df56875887110d
I-Use-Dial-Up <danielgonzalez@hilltoprobotics.com>
2026-05-09 14:34:32 -0700

    Made the score manager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro; // Required for TextMeshPro
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance; // Easy access from other scripts
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
